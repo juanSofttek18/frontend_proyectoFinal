4c08782f5d714e501d13c1c43601bf0b287e7afd
SOFTTEKPROJECTS\juand.castillo <juand.castillo@softtek.com>
2026-05-18 16:20:47 +0200

    Corregido los imports de App.jsx Y main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Router } from "react-router-dom"
+import { Route, Routes } from "react-router-dom"
 import SideBar from "./components/SideBar"
 import DashBoard from "./views/DashBoard/DashBoard"
 import "./App.css"
@@ -9,19 +9,17 @@ const requestsPlaceHolder= () => <h2 className="placeholder-text">Requests</h2>;
 
 function App() {
   return (
-    <BrowserRouter>
       <div className="app-container">
         <SideBar />
         <main className="main-content">
-          <Router>
+          <Routes>
             <Route path="/dashboard" element={<DashBoard />} />
             <Route path="/vehicles" element={<vehiclesPlaceHolder />} />
             <Route path="/clients" element={<clientsPlaceHolder />} />
             <Route path="/requests" element={<requestsPlaceHolder />} />
-          </Router>
+          </Routes>
         </main>
       </div>
-    </BrowserRouter>
   )
 }
 export default App
