b1c45e9622b6736d18fc9a098deb4d795ec905a5
SOFTTEKPROJECTS\juand.castillo <juand.castillo@softtek.com>
2026-05-18 16:57:51 +0200

    ARREGLADO EL MAIN Y APP

--- src/main.jsx
+++ src/main.jsx
@@ -11,10 +11,3 @@ createRoot(document.getElementById('root')).render(
     </BrowserRouter>
   </StrictMode>,
 )
-=======
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
->>>>>>> feature/SolitiudesView
