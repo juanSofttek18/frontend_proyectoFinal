3f14ecc8707629480734d87323d6db77019fc87d
Jaime Castillo <jaime.castillo@softtek.com>
2026-05-19 11:00:34 +0200

    VehicleCatalog añadido boton para ir a detalle del vehiculo pulsado (no probado)

--- src/views/Vehicles/VehicleCatalog.jsx
+++ src/views/Vehicles/VehicleCatalog.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate} from "react-router-dom";
 import { useCallback } from "react";
 import useVehicles from "./useVehicles";
 import { getAllAbleVehicles } from "../../services/vehiculoService";
@@ -6,11 +6,18 @@ import GenericTable from "../../components/GenericTable";
 
 function VehicleCatalog() {
   const params = useParams();
+  const navigate = useNavigate();
+
+
   const apiFn = useCallback(() => {
     return getAllAbleVehicles(params.id);
   }, [params.id]);
 
   const [vehicles, vehiclesStatus, vehiclesError] = useVehicles(apiFn);
+
+    function handleGoToDetail(vehicleId) {
+    navigate(`/vehicles/${vehicleId}`);
+  }
 
   if (vehiclesStatus === "loading") {
     return <p>Cargando vehículos...</p>;
@@ -42,6 +49,7 @@ function VehicleCatalog() {
           "Plazas",
           "Cuota Mensual Base",
           "Disponibilidad",
+          "Acciones",
         ]}
       >
         {vehicles.map((vehicle) => (
@@ -50,12 +58,20 @@ function VehicleCatalog() {
             <td>{vehicle.brand}</td>
             <td>{vehicle.model}</td>
             <td>{vehicle.price} €</td>
-            <td>{vehicle.engine_size}</td>
+            <td>{vehicle.cc}</td>
             <td>{vehicle.potency}</td>
             <td>{vehicle.color}</td>
             <td>{vehicle.spots}</td>
             <td>{vehicle.base_monthly_fee} €</td>
             <td>{vehicle.available ? "Sí" : "No"}</td>
+            <td>
+              <button
+                className="vehicle-detail-button"
+                onClick={() => handleGoToDetail(vehicle.id)}
+              >
+                Ver detalle
+              </button>
+            </td>
           </tr>
         ))}
       </GenericTable>
