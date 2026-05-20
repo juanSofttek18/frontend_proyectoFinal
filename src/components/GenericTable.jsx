f82d74c82ba7c52c006bd27d88e63073c84db5ab
SOFTTEKPROJECTS\juand.castillo <juand.castillo@softtek.com>
2026-05-20 11:13:12 +0200

    RESPONSIVE HECHO

--- src/components/GenericTable.jsx
+++ src/components/GenericTable.jsx
@@ -1,17 +1,17 @@
 function GenericTable({ headers, children }) {
-return(
-    <div>
-        <table className="generic-table">
-            <thead>
-                <tr>
-                    {headers.map((header, index) => (
-                        <th key={index}>{header}</th>
-                    ))}
-                </tr>
-            </thead>
-            <tbody>{children}</tbody>
-        </table>
+  return (
+    <div className="table-responsive-wrapper">
+      <table className="generic-table">
+        <thead>
+          <tr>
+            {headers.map((header, index) => (
+              <th key={index}>{header}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>{children}</tbody>
+      </table>
     </div>
-);
+  );
 }
 export default GenericTable;
